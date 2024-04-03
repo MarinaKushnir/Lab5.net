d01354f468a8b08cc9153107f40156e3d49a4a1b
MarinaKushnir <80197488+MarinaKushnir@users.noreply.github.com>
2024-04-03 18:35:53 +0300

    Update Wind.cs

--- Wind.cs
+++ Wind.cs
@@ -1,12 +1,36 @@
-﻿namespace Lab5
+namespace Lab5
 {
     internal class Wind : MusicInstrument
 {
-    public string InstrumentType { get; set; }
-    public string MouthpieceMaterial { get; set; }
-    public string InstrumentMaterial { get; set; }
+        private string _instrumentType;
+        private string _mouthpieceMaterial;
+        private string _instrumentMaterial;
 
-    public override string ToString()
+        public string InstrumentType
+        {
+            get => _instrumentType;
+            set => _instrumentType = value.Trim();
+        }
+        public string MouthpieceMaterial
+        {
+            get => _mouthpieceMaterial;
+            set => _mouthpieceMaterial = value.Trim();
+        }
+        public string InstrumentMaterial
+        {
+            get => _instrumentMaterial;
+            set => _instrumentMaterial = value.Trim();
+        }
+
+
+        public Wind(string name, string maker, string color, double weight, int price, string instrumentType, string mouthpieceMaterial, string instrumentMaterial) : base(name, maker, color, weight, price)
+        {
+            InstrumentType = instrumentType;
+            MouthpieceMaterial = mouthpieceMaterial;
+            InstrumentMaterial = instrumentMaterial;
+        }
+
+        public override string ToString()
     {
         return $"{base.ToString()} \n Тип инструмента: {InstrumentType};\n Материал мундштука: {MouthpieceMaterial};\n Материал инструмента: {InstrumentMaterial}.";
     }
