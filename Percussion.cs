8cf12b416e43cd3a9f23408e23b965ffefb00f4d
MarinaKushnir <80197488+MarinaKushnir@users.noreply.github.com>
2024-04-03 18:35:16 +0300

    Update Percussion.cs

--- Percussion.cs
+++ Percussion.cs
@@ -1,10 +1,35 @@
-﻿namespace Lab5
+namespace Lab5
 {
     internal class Percussion : MusicInstrument
     {
-        public int NumberOfPieces { get; set; }
-        public string DrumType { get; set; }
-        public string DrumMaterial { get; set; }
+
+        private string _drumType;
+        private string _drumMaterial;
+        private int _numberOfPieces;
+        public static readonly int MAX_NumberOfPieces = 100;
+        public int NumberOfPieces
+        {
+            get => _numberOfPieces;
+            set => _numberOfPieces = value > 0 && value < MAX_NumberOfPieces ? value : throw new System.ArgumentOutOfRangeException();
+        }
+        public string DrumType{ 
+            get=> _drumType; 
+            set => _drumType = value.Trim(); 
+        }
+        public string DrumMaterial{
+            get => _drumMaterial;
+            set => _drumMaterial = value.Trim();
+        }
+
+
+        public Percussion(string name, string maker, string color, double weight, int price, int numberOfPieces, string drumType, string drumMaterial) : base(name, maker, color, weight, price)
+        {
+            NumberOfPieces= numberOfPieces;
+            DrumType = drumType;
+            DrumMaterial = drumMaterial;
+        }
+
+        
 
         public override string ToString()
         {
