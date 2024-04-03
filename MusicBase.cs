d5ec32b5705ff18fff15dc244e3352ec1975f76d
MarinaKushnir <80197488+MarinaKushnir@users.noreply.github.com>
2024-04-03 18:34:43 +0300

    Update MusicBase.cs

--- MusicBase.cs
+++ MusicBase.cs
@@ -1,16 +1,50 @@
-﻿namespace Lab5
+namespace Lab5
 {
-    internal class MusicBaseInfo
+    internal class MusicBase
     {
-        public string Name { get; set; }
-        public string Maker { get; set; }
-        public string Color { get; set; }
-        public double Weight { get; set; }
-        public int Price { get; set; }
+        private string _name;
+        private string _maker;
+        private string _color;
+        private double _weight;
+        private int _price;
+        public static readonly int MAX_Weight = 100;
 
+        public string Name { 
+            get=> _name; 
+            set => _name = value.Trim(); 
+        }
+        public string Maker
+        {
+            get => _maker;
+            set => _maker = value.Trim();
+        }
+        public string Color
+        {
+            get => _color;
+            set => _color = value.Trim();
+        }
+        public double Weight
+        {
+            get => _weight;
+            set => _weight = value > 0 && value< MAX_Weight ? value : throw new System.ArgumentOutOfRangeException();
+        }
+        public int Price
+        {
+            get => _price;
+            set => _price = value > 0 ? value : throw new System.ArgumentOutOfRangeException();
+        }
+
+        public MusicBase(string name, string maker, string color, double weight, int price)
+        {
+            Name = name;
+            Maker = maker;
+            Color = color;
+            Weight = weight;
+            Price = price;
+        }
         public override string ToString()
         {
-            return $"\n Имя: {Name}; \n Производитель: {Maker};\n Цвет: {Color};\n Вес: {Weight};\n Цена(грн): {Price};";
+            return $"\n Имя: {Name}; \n Производитель: {Maker};\n Цвет: {Color};\n Вес: {Weight} кг;\n Цена: {Price} грн;";
         }
     }
 }
