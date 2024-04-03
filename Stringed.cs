450f41ccec6de0e82864d65a50b81c5f95d9d8b6
MarinaKushnir <80197488+MarinaKushnir@users.noreply.github.com>
2024-04-03 18:35:39 +0300

    Update Stringed.cs

--- Stringed.cs
+++ Stringed.cs
@@ -1,10 +1,36 @@
-﻿namespace Lab5
+namespace Lab5
 {
     internal class Stringed : MusicInstrument
     {
-        public int NumberOfStrings { get; set; }
-        public string StringMaterial { get; set; }
-        public string IsElectric { get; set; }
+        private string _isElectric;
+        private string _stringMaterial;
+        private int _numberOfStrings;
+        public static readonly int MAX_NumberOfStrings = 50;
+
+        public int NumberOfStrings
+        {
+            get => _numberOfStrings;
+            set => _numberOfStrings = value > 0 && value < MAX_NumberOfStrings ? value : throw new System.ArgumentOutOfRangeException();
+        }
+        public string IsElectric
+        {
+            get => _isElectric;
+            set => _isElectric = value.Trim();
+        }
+        public string StringMaterial
+        {
+            get => _stringMaterial;
+            set => _stringMaterial = value.Trim();
+        }
+
+
+        public Stringed(string name, string maker, string color, double weight, int price, int numberOfStrings, string isElectric, string stringMaterial) : base(name, maker, color, weight, price)
+        {
+            NumberOfStrings = numberOfStrings;
+            IsElectric = isElectric;
+            StringMaterial = stringMaterial;
+        }
+
 
         public override string ToString()
         {
