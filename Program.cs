0a24eef5e43e8ae13f556c4d46b7adae4aa7fc59
MarinaKushnir <80197488+MarinaKushnir@users.noreply.github.com>
2024-04-14 18:32:41 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Lab5
         static void Main(string[] args)
         {
             Percussion drum1 = new Percussion("Барабан", "Mapex", "Коричневый", 4, 6082, 1, "Малый барабан", "Дерево");
-            Stringed guitar1 = new Stringed("Гитара", "GEWA", "Черный", 3.5, 4322, 6, "Нейлон", "Есть");
+            Stringed guitar1 = new Stringed("Гитара", "GEWA", "Черный", 3.5, 4322, 6, true, "Нейлон");
             Wind flute1 = new Wind("Флейта", "Mooer", "Белый", 0.5, 10628, "Блок-флейта", "Пластик", "Пластик");
 
             MusicInstrument[] instruments = { drum1, guitar1, flute1 };
